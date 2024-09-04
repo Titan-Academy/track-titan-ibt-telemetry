c3a3c0920d619f3aef19c0f1348e7639bdd1cc64
WolfgangReip <wolf.reip@gmail.com>
2024-09-04 11:35:46 +0200

    fixing yml error for iRacing and updating package

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,11 +1,24 @@
-import fs from 'fs'
-import yaml from 'js-yaml'
+import fs from 'fs';
+import yaml from 'js-yaml';
 
-import TelemetrySample from './telemetry-sample'
-import telemetryFileLoader from './utils/telemetry-file-loader'
+import TelemetrySample from './telemetry-sample';
+import telemetryFileLoader from './utils/telemetry-file-loader';
 
-const variableHeaders = new WeakMap()
-const fileDescriptor = new WeakMap()
+const variableHeaders = new WeakMap();
+const fileDescriptor = new WeakMap();
+
+function handleMultilineStrings(sessionInfo) {
+  // Correctly handle multiline values with block scalar and proper indentation
+  sessionInfo = sessionInfo.replace(
+    /:\s*\|\n\s*([^\n]+(\n[^\n]+)+)/g,
+    function (match, value) {
+      // Format only multiline values with block scalars
+      return `: |\n  ${value.replace(/\n/g, '\n  ')}`;
+    }
+  );
+
+  return sessionInfo;
+}
 
 /**
  * iRacing Telemetry
@@ -14,19 +27,24 @@ export default class Telemetry {
   /**
    * Telemetry constructor.
    */
-  constructor (telemetryHeader, diskSubHeader, sessionInfo, varHeaders, fd) {
-    this.headers = telemetryHeader
-    this.diskHeaders = diskSubHeader
+  constructor(telemetryHeader, diskSubHeader, sessionInfo, varHeaders, fd) {
+    this.headers = telemetryHeader;
+    this.diskHeaders = diskSubHeader;
 
     const sanitizedSessionInfo = sessionInfo.replace(
       // eslint-disable-next-line no-control-regex
       /[\x00-\x08\x0B\x0C\x0E-\x1F\x7F\x80-\x9F]/g,
       ''
-    )
-    this.sessionInfo = yaml.load(sanitizedSessionInfo)
+    );
 
-    fileDescriptor.set(this, fd)
-    variableHeaders.set(this, varHeaders)
+    // Step 2: Fix indentation for multiline strings
+    const handledMultilineStringsSessionInfo =
+      handleMultilineStrings(sanitizedSessionInfo);
+
+    this.sessionInfo = yaml.load(handledMultilineStringsSessionInfo);
+
+    fileDescriptor.set(this, fd);
+    variableHeaders.set(this, varHeaders);
   }
 
   /**
@@ -35,36 +53,36 @@ export default class Telemetry {
    * @param file path to *.ibt file
    * @return Telemetry instance of telemetry
    */
-  static fromFile (file) {
-    return telemetryFileLoader(file)
+  static fromFile(file) {
+    return telemetryFileLoader(file);
   }
 
   /**
    * Telemetry variable headers.
    */
-  get varHeaders () {
-    return variableHeaders.get(this)
+  get varHeaders() {
+    return variableHeaders.get(this);
   }
 
   /**
    * Telemetry samples generator.
    */
-  * samples () {
-    let hasSample = true
-    let count = 0
+  *samples() {
+    let hasSample = true;
+    let count = 0;
 
-    const fd = fileDescriptor.get(this)
-    const length = this.headers.bufLen
-    const buffer = Buffer.alloc(length)
+    const fd = fileDescriptor.get(this);
+    const length = this.headers.bufLen;
+    const buffer = Buffer.alloc(length);
 
     while (hasSample) {
-      const start = this.headers.bufOffset + count++ * length
-      const bytesRead = fs.readSync(fd, buffer, 0, length, start)
+      const start = this.headers.bufOffset + count++ * length;
+      const bytesRead = fs.readSync(fd, buffer, 0, length, start);
 
       if (bytesRead !== length) {
-        hasSample = false
+        hasSample = false;
       } else {
-        yield new TelemetrySample(buffer, this.varHeaders)
+        yield new TelemetrySample(buffer, this.varHeaders);
       }
     }
   }
